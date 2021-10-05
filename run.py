5a8e7ae2e84454d9128c500072c7ea64a2d1758e
insmnia <teamspirit3209@gmail.com>
2021-10-05 23:58:48 +0300

    f

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import create_app, db
-from app.models import User, Order
+from app.models import User, Order, Stop, Day
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Order': Order}
+    return {'db': db, 'User': User, 'Order': Order,"Stop":Stop,"Day":Day}
