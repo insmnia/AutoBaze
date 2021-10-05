53aab434cf980212c3f16e6a72b1ab87af233b19
insmnia <teamspirit3209@gmail.com>
2021-10-05 12:16:10 +0300

    f

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "super key"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI') or 'postgres://nhmzonsxmrjxdv:9691e189db82b72c14e194ee2f85c20d680d6062d338eaca91394efbe07bcbe6@ec2-52-7-228-45.compute-1.amazonaws.com:5432/d5d51dg6r2gpl0'
+        'SQLALCHEMY_DATABASE_URI') or 'postgresql://nhmzonsxmrjxdv:9691e189db82b72c14e194ee2f85c20d680d6062d338eaca91394efbe07bcbe6@ec2-52-7-228-45.compute-1.amazonaws.com:5432/d5d51dg6r2gpl0'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     ADMIN = ["jlava402@gmail.com"]
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
