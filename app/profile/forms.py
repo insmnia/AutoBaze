7232c3b6b1f1546de4d7a5e948b0eec20050abde
insmnia <teamspirit3209@gmail.com>
2021-10-08 00:50:49 +0300

    Password validation

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import ValidationError, DataRequired, Email
+from wtforms.validators import ValidationError, DataRequired, Email, Length
 from app.models import User
 import re
 
+
 class ChangePasswordForm(FlaskForm):
     current_password = PasswordField(
-        "Старый пароль", validators=[DataRequired(message="Это поле обязательно для заполнения")])
-    new_password = PasswordField("Новый пароль", validators=[DataRequired(message="Это поле обязательно для заполнения")])
+        "Старый пароль", validators=[DataRequired(message="Это поле обязательно для заполнения"), Length(min=8, max=20, message="Длина пароля должна быть от 8-ми до 20-ти символов")])
+    new_password = PasswordField("Новый пароль", validators=[
+                                 DataRequired(message="Это поле обязательно для заполнения")])
     submit = SubmitField("Сменить")
 
 
@@ -16,7 +18,7 @@ class ChangeEmailForm(FlaskForm):
         "Пароль", validators=[DataRequired(message="Это поле обязательно для заполнения")])
     new_email = StringField("Новая почта", validators=[
                             DataRequired(message="Это поле обязательно для заполнения"), Email(
-        granular_message=True, check_deliverability=True, message="Проверьте введенные данные")])
+                                granular_message=True, check_deliverability=True, message="Проверьте введенные данные")])
     submit = SubmitField("Сменить")
 
     def validate_email(self, email):
